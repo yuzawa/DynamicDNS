538da52728e8d9e4fc88a079fe261c88941cb8ff
Manabu Yuzawa <yuzawa@waltz.plala.or.jp>
2015-11-05 11:47:32 +0900

    Real Environment

--- getmyip.py
+++ getmyip.py
@@ -7,9 +7,9 @@ from syslog import syslog
 # ======= CONFIG ========
 AWS_ACCESS_KEY_ID = 'AKIAIYJFCYZ5YGZFBGFQ'
 AWS_SECRET_ACCESS_KEY = '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'
-AWS_R53_ADDR = "test.manabu-yuzawa.org." # Should end in period
+AWS_R53_ADDR = "home.manabu-yuzawa.org." # Should end in period
 AWS_R53_ZONE  = "Z69OGRXUK53QL"
-GET_IP_URL = "http://fedora22.manabu-yuzawa.org/cgi-bin/ip.py"
+GET_IP_URL = "http://fedora.manabu-yuzawa.org/cgi-bin/ip.py"
 # ===== END CONFIG ======
 
 ip = urllib2.urlopen(GET_IP_URL).read().strip()
